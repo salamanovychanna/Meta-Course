f0e6aecadcb06d3294ba3eac9fd3f88485dc0d51
enndesigner <anna20080322@gmail.com>
2024-01-05 18:28:53 +0200

    OG meta tags changes

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -2,7 +2,15 @@
 const Header = () => {
     return (
         <header>
-            
+            logo
+            <nav> 
+                <li>Home</li>
+                <li>About</li>
+                <li>Menu</li>
+                <li>Reservations</li>
+                <li>Order online</li>
+                <li>Login</li>
+            </nav>
         </header>
     );
 };
